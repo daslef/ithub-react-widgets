af32515e1ff416e43531b458b29a919ee99d34a5
Lex Trofimov <venusinleo@yandex.ru>
2025-09-11 18:06:11 +0300

    refac: move widget-smartdesk to css modules

--- src/widgets/widget-smartdesk/WidgetSmartdesk.jsx
+++ src/widgets/widget-smartdesk/WidgetSmartdesk.jsx
@@ -1,18 +1,21 @@
-import './WidgetSmartdesk.css'
+import classes from './WidgetSmartdesk.module.css'
 
 export default function WidgetSmartdesk(props) {
     return (
-        <article className="widget widget--smartdesk">
+        <article className={`widget ${classes["widget--smartdesk"]}`}>
             <button type="button" onClick={() => props.setPage('quote')}>To quote</button>
-            <div className="header">
-                <p className="header__office">Office #1</p>
-                <h2 className="header__heading">Smart desk</h2>
+            <div className={classes.header}>
+                <p className={classes.header__office}>Office #1</p>
+                <h2 className={classes.header__heading}>Smart desk</h2>
             </div>
-            <section className="main">
-                <p className="main__height">85cm</p>
-                <p className="main__heading">Current height</p>
+            <section className={classes.main}>
+                <p className={classes.main__height}>85cm</p>
+                <p className={classes.main__heading}>Current height</p>
             </section>
-            <section className="footer"></section>
+            <section className={classes.footer}>
+                <button className={`${classes.footer__button} ${classes["footer__button--left"]}`}>-</button>
+                <button className={`${classes.footer__button} ${classes["footer__button--right"]}`}>+</button>
+            </section>
         </article>
     )
 }
