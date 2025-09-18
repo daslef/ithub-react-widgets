6caa3f43ef5acc8bdcafbda31d498c3851fa71b7
Lex Trofimov <daslef93@gmail.com>
2025-09-18 16:23:25 +0300

    wip: widget-menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import WidgetQuote from './widgets/widget-quote/WidgetQuote'
 import WidgetPooltime from './widgets/widget-pooltime/WidgetPooltime'
 import WidgetSmartdesk from './widgets/widget-smartdesk/WidgetSmartdesk'
+import WidgetMenu from './widgets/widget-menu/WidgetMenu'
 
 import './App.css'
 
 export default function App() {
-    const [page, setPage] = useState("smartdesk")
+    const [page, setPage] = useState("menu")
 
     const pages = {
+        menu: <WidgetMenu setPage={setPage} />,
         quote: <WidgetQuote setPage={setPage} />,
         pooltime: <WidgetPooltime setPage={setPage} />,
         smartdesk: <WidgetSmartdesk setPage={setPage} />
--- src/widgets/widget-menu/WidgetMenu.jsx
+++ src/widgets/widget-menu/WidgetMenu.jsx
@@ -1,24 +1,19 @@
 import classes from './WidgetMenu.module.css'
+import quoteIcon from '../../assets/icons/quote-double.svg'
+import smartDeskIcon from '../../assets/icons/table.svg'
 
 export default function WidgetMenu(props) {
 
 
     return (
         <article className={`widget ${classes["widget--menu"]}`}>
-            <button type="button" onClick={() => props.setPage('quote')}>To quote</button>
-            <button type="button" onClick={() => props.setPage('pooltime')}>To pooltime</button>
-            <div className={classes.header}>
-                <p className={classes.header__office}>Office #1</p>
-                <h2 className={classes.header__heading}>Smart desk</h2>
-            </div>
-            <section className={classes.main}>
-                <p className={classes.main__height}>{value}cm</p>
-                <p className={classes.main__heading}>Current height</p>
-            </section>
-            <section className={classes.footer}>
-                <button onClick={decrease} className={decreaseButtonClasses}>-</button>
-                <button onClick={increase} className={increaseButtonClasses}>+</button>
-            </section>
+            <button className={classes.button} type="button" onClick={() => props.setPage('quote')}>
+                <img src={quoteIcon} alt="quote" className={classes.button__icon} />
+            </button>
+            <button className={classes.button} type="button" onClick={() => props.setPage('smartdesk')}>
+                <img src={smartDeskIcon} alt="smartdesk" className={classes.button__icon} />
+            </button>            
+            <button type="button" onClick={() => props.setPage('pooltime')}>pooltime</button>
         </article>
     )
 }
