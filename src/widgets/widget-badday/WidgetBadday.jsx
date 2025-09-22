fc2e9d0789ee231e18f9afadf76b57b0bf8604ee
Трофимов Алексей <a.trofimov@domain.local>
2025-09-22 19:45:34 +0300

    wip: badday

--- src/widgets/widget-badday/WidgetBadday.jsx
+++ src/widgets/widget-badday/WidgetBadday.jsx
@@ -8,17 +8,23 @@ const baddaysData = [
     { id: 4, isBad: false },
 ]
 
+
 export default function WidgetBadday(props) {
+    function changeIsBad(event) {
+        const dayId = Number(event.target.id)
+        const dayObject = baddaysData.find(day => day.id === dayId)
+        dayObject.isBad = !dayObject.isBad
+    }
 
     return (
         <article className={`widget ${classes["widget--badday"]}`}>
             <main className={classes.main}>
                 {baddaysData.map(day => {
-                    if (day.isBad) {
-                        return <button className={classes.button_bad}></button>
-                    } else {
-                        return <button className={classes.button}></button>
-                    }
+                    const classNames = day.isBad
+                        ? `${classes.button} ${classes.button_bad}`
+                        : `${classes.button}`
+
+                    return <button key={day.id} id={day.id} onClick={changeIsBad} className={classNames}></button>
                 })}
             </main>
             <footer className={classes.footer}></footer>
